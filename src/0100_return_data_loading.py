f8462f80c95ed5d8f8273ee9281288f6f3d318a2
Alexander Steeb <alexander.steeb@gmx.de>
2019-02-26 20:01:31 +0100

    Added Jupyter notebooks

---
+++ src/0100_return_data_loading.py
@@ -0,0 +1,42 @@
+# Description
+#
+#
+#
+
+import wrds
+import feather
+
+# Connect to DB
+db = wrds.Connection(wrds_username='amglex')
+print('Successfully connected')
+
+
+######################
+# Query the data
+######################
+
+
+print('Start downloading data ...')
+
+# SQL Query
+data_raw_df = db.raw_sql(
+    '''
+    SELECT * 
+    FROM monthly_returns
+    WHERE caldt > '2018-10-01'
+    '''
+)
+
+data_raw_df = data_raw_df.copy()
+
+print('SQL successful')
+
+
+print(data_raw_df.head())
+
+# path = 'data/data_df.feather'
+path = '../data/monthly_returns.feather'
+
+feather.write_dataframe(data_raw_df, path)
+
+print("Successfully saved data")
