ac3a919919a78ad84f32e8978bc8953d72d8c264
Alexander Steeb <alexander.steeb@gmx.de>
2019-03-06 03:02:51 +0100

    Added an slightly changed file for loading holdings

---
+++ src/data/0100_data_loading.py
@@ -0,0 +1,47 @@
+# Description
+#
+#
+#
+
+import wrds
+import feather
+
+# Connect to DB
+db = wrds.Connection(wrds_username='amglex')
+print('Successfully connected')
+
+
+######################
+# Query the data
+######################
+
+
+print('Start downloading data ...')
+
+# SQL Query
+data_raw_df = db.raw_sql(
+    '''
+    SELECT crsp_portno, report_dt, percent_tna, nbr_shares, market_val, crsp_company_key
+    FROM holdings
+    '''
+)
+
+print('SQL successful')
+
+print(data_raw_df.dtypes)
+
+data_raw_df['crsp_portno'] = data_raw_df['crsp_portno'].astype(int)
+data_raw_df['crsp_company_key'] = data_raw_df['crsp_company_key'].astype(int)
+
+
+print(data_raw_df.dtypes)
+
+
+print(data_raw_df.head())
+
+# path = 'data/data_df.feather'
+path = '../../data/raw/holdings.feather'
+
+feather.write_dataframe(data_raw_df, path)
+
+print("Successfully saved data")
