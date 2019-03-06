4dfe1e72019a9a837b7f8c25b8df2a3f641f844e
Alexander Steeb <alexander.steeb@gmx.de>
2019-03-07 00:30:07 +0100

    Copied file containing sql command over

---
+++ src/data/0100_data_loading_total_summary.py
@@ -0,0 +1,49 @@
+# Description
+#
+#
+#
+
+import wrds
+import feather
+import pandas as pd
+
+# Connect to DB
+db = wrds.Connection(wrds_username='amglex')
+print('Successfully connected')
+
+
+######################
+# Query the data
+######################
+
+
+print('Start downloading data ...')
+
+# SQL Query
+data_raw_df = db.raw_sql(
+    '''
+    SELECT hdr.crsp_fundno, hdr.crsp_portno, 
+        first_offer_dt, index_fund_flag, et_flag, 
+        begdt, enddt, crsp_obj_cd
+    FROM fund_hdr hdr
+    FULL JOIN fund_style style 
+    ON hdr.crsp_fundno = style.crsp_fundno 
+    '''
+)
+
+print('SQL successful')
+
+
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print(data_raw_df.head())
+
+print(data_raw_df.head())
+
+print(data_raw_df.shape)
+
+# path = 'data/data_df.feather'
+path = '../../data/raw/total_summary_new.feather'
+
+feather.write_dataframe(data_raw_df, path)
+
+print("Successfully saved data")
