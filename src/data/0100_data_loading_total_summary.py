5d504f272a8f3078fefbafe040c7b7fcb2edb020
Alexander Steeb <alexander.steeb@gmx.de>
2019-07-29 01:46:28 +0200

    update
     Added first new Notebook for new data loading

--- src/data/0100_data_loading_total_summary.py
+++ src/data/0100_data_loading_total_summary.py
@@ -24,7 +24,7 @@ print('Start downloading data ...')
 
 data_raw_df = db.raw_sql(
     '''
-    SELECT hdr.crsp_fundno, hdr.crsp_portno,
+    SELECT hdr.crsp_fundno, hdr.crsp_portno, hdr.fund_name,
         first_offer_dt, index_fund_flag, et_flag,
         begdt, enddt, lipper_class, avrcs
     FROM fund_hdr hdr
@@ -39,33 +39,12 @@ data_raw_df = db.raw_sql(
     ON style.crsp_fundno = b.crsp_fundno;
     '''
 )
-#
-# data_raw_df = db.raw_sql(
-#     '''
-#     SELECT
-#         a.crsp_fundno, a.lipper_class, a.begdt, a.enddt
-#         b.avrcs
-#     FROM fund_style a
-#     LEFT JOIN
-#         (SELECT distinct
-#             crsp_fundno, sum(per_com)/count(per_com) as avrcs
-#         FROM crsp.fund_summary
-#         GROUP BY crsp_fundno) b
-#     ON a.crsp_fundno=b.crsp_fundno;
-#     '''
-# )
 
 print('SQL successful')
 
-
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    print(data_raw_df['lipper_class'].sample(10))
-
 print(data_raw_df.shape)
 
-# path = 'data/data_df.feather'
 path = '../../data/raw/total_summary_new.feather'
-
 feather.write_dataframe(data_raw_df, path)
 
 print("Successfully saved data")
